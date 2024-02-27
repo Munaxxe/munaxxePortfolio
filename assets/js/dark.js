18152487a6762993490361aecf5fce620e4e16be
i_am_mUNAxxe <85413674+Munaxxe@users.noreply.github.com>
2024-02-27 23:56:31 +0200

    Update dark.js

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,6 +1,6 @@
 document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
-const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
-const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+const sun = "sun.svg";
+const moon = "moon.svg";
 
 // var theme = "dark";
 var theme = "light";
